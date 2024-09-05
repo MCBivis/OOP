834d261bfefc53752d04464b12b53e5eae163ec6
MCBivis <Artemiybox@mail.ru>
2024-09-05 13:57:46 +0700

    Trying to avoid checkstyle error

--- Task_1_1_1/src/test/java/org/example/HeapSortTest.java
+++ Task_1_1_1/src/test/java/org/example/HeapSortTest.java
@@ -2,24 +2,24 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class HeapSortTest {
 
-    @Test
+  @Test
     void heapsort() {
-        int[] arr= {5,4,3,2,1};
-        int[] arr2= {0};
-        int[] arr3= {};
-        HeapSort.heapSort(arr);
-        HeapSort.heapSort(arr2);
-        HeapSort.heapSort(arr3);
-        int[] expectArr = {1,2,3,4,5};
-        assertArrayEquals(arr,expectArr);
-        int[] expectArr2 = {0};
-        assertArrayEquals(arr2,expectArr2);
-        int[] expectArr3 = {};
-        assertArrayEquals(arr3,expectArr3);
-    }
+    int[] arr = {5, 4, 3, 2, 1};
+    int[] arr2 = {0};
+    int[] arr3 = {};
+    HeapSort.heapSort(arr);
+    HeapSort.heapSort(arr2);
+    HeapSort.heapSort(arr3);
+    int[] expectArr = {1, 2, 3, 4, 5};
+    assertArrayEquals(arr, expectArr);
+    int[] expectArr2 = {0};
+    assertArrayEquals(arr2, expectArr2);
+    int[] expectArr3 = {};
+    assertArrayEquals(arr3, expectArr3);
+  }
 
 }
