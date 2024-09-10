634de9affc7cfddd6a4bbac0db61bf2e126b7950
MCBivis <Artemiybox@mail.ru>
2024-09-10 21:27:32 +0700

    Update Blackjack and Deck

--- Task_1_1_2/src/main/java/org/example/Blackjack.java
+++ Task_1_1_2/src/main/java/org/example/Blackjack.java
@@ -1,30 +1,14 @@
 package org.example;
 
-
 public class Blackjack {
     public static void main(String[] args) {
 
         System.out.println("Добро пожаловать в Блэкджек!");
 
-        Card[] deck = new Card[52];
-
-        for (int j = 0; j < 4; j++) {
-            for (int i = 0; i < 13; i++) {
-                String name = Card.NAMES[i];
-                String suit = Card.SUITS[j];
-                int value;
-                if (i<9){
-                    value=i+2;
-                }else if (i<12){
-                    value=10;
-                }else{
-                    value=11;
-                }
-                deck[i+13*j] =new Card(suit,name,value);
-            }
-        }
+        Deck deck = new Deck();
+        deck.shuffle();
         for (int j = 0; j < 52; j++) {
-            System.out.println(deck[j].CardShow());
+            System.out.println(deck.deck[j].CardShow());
         }
     }
 }
--- Task_1_1_2/src/main/java/org/example/Deck.java
+++ Task_1_1_2/src/main/java/org/example/Deck.java
@@ -1,4 +1,36 @@
 package org.example;
 
+import java.util.Random;
+
 public class Deck {
+
+    Card[] deck;
+    private static final Random random = new Random();
+    public Deck() {
+        deck = new Card[52];
+        for (int j = 0; j < 4; j++) {
+            for (int i = 0; i < 13; i++) {
+                String name = Card.NAMES[i];
+                String suit = Card.SUITS[j];
+                int value;
+                if (i<9){
+                    value=i+2;
+                }else if (i<12){
+                    value=10;
+                }else{
+                    value=11;
+                }
+                deck[i+13*j] =new Card(suit,name,value);
+            }
+        }
+    }
+    public void shuffle() {
+        for(int i= deck.length-1; i>=0; i--) {
+            int index = random.nextInt(i + 1);
+            System.out.println(index);
+            Card temp = deck[i];
+            deck[i] = deck[index];
+            deck[index] = temp;
+        }
+    }
 }
