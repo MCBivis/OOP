cccee0598ef44c48a84a527baf10711e1052bf21
MCBivis <Artemiybox@mail.ru>
2024-09-12 17:09:56 +0700

    Added comments and script

---
+++ Task_1_1_2/src/test/java/org/example/CardTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardTest {
+
+    @Test
+    void cardShow() {
+        Card card = new Card("Червы","Двойка",2);
+        String card1 = "Двойка Червы (2)";
+        assertEquals(card.CardShow(), card1,"Функция должна выводить строку, ожидаемого формата.");
+    }
+}
