db1502c97ded13ed93cfcb14711e04053f355e70
MCBivis <Artemiybox@mail.ru>
2024-09-24 18:57:03 +0700

    done all requests

--- Task_1_1_2/Build2/doc/type-search-index.js
+++ Task_1_1_2/Build2/doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"Blackjack"},{"p":"org.example","l":"Card"},{"p":"org.example","l":"Deck"},{"p":"org.example","l":"Hand"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"BlackJack"},{"p":"org.example","l":"Card"},{"p":"org.example","l":"Deck"},{"p":"org.example","l":"Hand"},{"p":"org.example","l":"Main"}];updateSearchResults();
--- Task_1_1_2/sh.bat
+++ Task_1_1_2/sh.bat
@@ -1,4 +1,4 @@
 javac -d Build2/classes src/main/java/org/example/*.java
-jar --create --file Build2/jar/Blackjack.jar --main-class=org.example.Blackjack -C Build2/classes .
+jar --create --file Build2/jar/Blackjack.jar --main-class=org.example.Main -C Build2/classes .
 javadoc -d Build2/doc src/main/java/org/example/*.java
 java -jar Build2/jar/Blackjack.jar
