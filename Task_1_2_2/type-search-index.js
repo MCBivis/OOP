2bc88e641f397403a113f70d7437dac985bd247f
MCBivis <MCBivis@users.noreply.github.com>
2024-11-06 07:54:10 +0000

    Deploying to gh-pages from @ MCBivis/OOP@38dbd53fca277167365d923adb2a366778597e6f 🚀

--- Task_1_2_2/type-search-index.js
+++ Task_1_2_2/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"HashTable.Entry"},{"p":"org.example","l":"HashTable"},{"p":"org.example","l":"Main"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"Entry"},{"p":"org.example","l":"HashTable"},{"p":"org.example","l":"Main"}]
