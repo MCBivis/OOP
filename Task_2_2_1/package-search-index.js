b125896abcfcff1544948473208a13443fbf5742
MCBivis <MCBivis@users.noreply.github.com>
2025-03-18 11:14:14 +0000

    Deploying to gh-pages from @ MCBivis/OOP@302b4ced26873c4c3db4943b44a9f2d7391bfafa 🚀

--- Task_2_2_1/package-search-index.js
+++ Task_2_2_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.example"},{"l":"org.example.console"},{"l":"org.example.logger"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.example"},{"l":"org.example.console"},{"l":"org.example.interfaces_abstractClasses"},{"l":"org.example.logger"}];updateSearchResults();
